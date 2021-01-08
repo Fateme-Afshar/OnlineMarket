2d8511b76e97a3a2283bac351f2b4f250c5735a6
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-08 01:20:23 -0800

    Implementation share preferences for saving login customer information.

--- app/src/main/java/com/example/onlinemarket/sharePref/ProductSharePref.java
+++ app/src/main/java/com/example/onlinemarket/sharePref/ProductSharePref.java
@@ -1,5 +1,31 @@
 package com.example.onlinemarket.sharePref;
 
-public class ProductSharePref {
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import com.example.onlinemarket.model.customer.Customer;
+import com.google.gson.Gson;
+
+public class ProductSharePref {
+    private static final String KEY_CUSTOMER="productIds";
+
+    public static Customer getCustomer(Context context) {
+        Gson gson=new Gson();
+        String json=getSharePreference(context).getString(KEY_CUSTOMER,null);
+        return gson.fromJson(json,Customer.class);
+    }
+
+    public void saveCustomer(Context context,Customer customer){
+        Gson gson=new Gson();
+        String json=gson.toJson(customer);
+        getSharePreference(context).
+                edit().
+                putString(KEY_CUSTOMER,json).
+                apply();
+    }
+
+    private static SharedPreferences getSharePreference(Context context){
+        return context.getApplicationContext().
+                getSharedPreferences("com.example.onlinemarket",Context.MODE_PRIVATE);
+    }
 }
