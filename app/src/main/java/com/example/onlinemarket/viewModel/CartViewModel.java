fc72dbb4ec187452da7d85dc96b07cfb21911b74
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-09 11:28:03 -0800

    Adding finalize purchase button in cart layout and setup this in cart view model.

--- app/src/main/java/com/example/onlinemarket/viewModel/CartViewModel.java
+++ app/src/main/java/com/example/onlinemarket/viewModel/CartViewModel.java
@@ -9,7 +9,6 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 
 import com.example.onlinemarket.OnlineShopApplication;
 import com.example.onlinemarket.R;
@@ -24,7 +23,7 @@ import com.example.onlinemarket.utils.ProgramUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CartViewModel extends ViewModel {
+public class CartViewModel extends AndroidViewModel {
     private ProductPurchasedRepository mRepository= OnlineShopApplication.getProductPurchasedRepository();
     private int mTotalPrice=0;
 
@@ -32,9 +31,57 @@ public class CartViewModel extends ViewModel {
 
     private LifecycleOwner mLifecycleOwner;
 
+    private Orders mOrders;
+    private Customer mCustomer;
+
+    public CartViewModel(@NonNull Application application) {
+        super(application);
+        mCustomer=OnlineShopSharePref.getCustomer(getApplication());
+    }
 
     public LiveData<List<Product>> getProductRegisteredForPurchase(){
         return mRepository.getList();
+    }
+
+    public void onBuyBtnClickListener(){
+        List<LineItemsItem> productLines=new ArrayList<>();
+
+        for (Product product : mProductList) {
+            LineItemsItem lineItemsItem=new LineItemsItem(
+                    String.valueOf(product.getRegularPrice()),
+                    (int) product.getPrice(),
+                    product.getId(),
+                    product.getName());
+
+            productLines.add(lineItemsItem);
+        }
+        mOrders=new Orders(String.valueOf(mTotalPrice),mCustomer.getId(),productLines,mCustomer.getUsername());
+
+        postOrdersToServer(mOrders);
+    }
+
+    private void postOrdersToServer(Orders orders) {
+        mRepository.postOrdersToServer(orders);
+        mRepository.getResponseCode().observe(mLifecycleOwner, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                if (integer==201) {
+                    Log.d(ProgramUtils.TAG,
+                            "CartViewModel : Orders post successfully" + integer);
+                    Toast.makeText(getApplication(),
+                            R.string.order_success,
+                            Toast.LENGTH_LONG).
+                            show();
+                } else {
+                    Log.e(ProgramUtils.TAG,
+                            "CartViewModel : Orders post fail response code is  " + integer);
+                    Toast.makeText(getApplication(),
+                            R.string.order_fail,
+                            Toast.LENGTH_LONG).
+                            show();
+                }
+            }
+        });
     }
 
     public List<Product> getProductList() {
