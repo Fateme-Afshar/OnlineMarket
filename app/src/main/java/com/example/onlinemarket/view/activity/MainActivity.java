6c2c825d4e423491a61ea2e46f14e288635a15d5
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-10 05:17:04 -0800

    Implementation ui for NotificationFragment.

--- app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
+++ app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
@@ -44,7 +44,8 @@ public class MainActivity extends SingleFragmentActivity
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_categories, R.id.nav_search,R.id.nav_filter,R.id.nav_user_account)
+                R.id.nav_home, R.id.nav_categories, R.id.nav_search,R.id.nav_filter,
+                R.id.nav_user_account,R.id.nav_notification)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
---
+++ app/src/main/java/com/example/onlinemarket/view/fragment/NotificationFragment.java
@@ -0,0 +1,48 @@
+package com.example.onlinemarket.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.onlinemarket.R;
+import com.example.onlinemarket.view.IOnBackPress;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotificationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotificationFragment extends Fragment implements IOnBackPress {
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+    public static NotificationFragment newInstance() {
+        NotificationFragment fragment = new NotificationFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        return true;
+    }
+}
