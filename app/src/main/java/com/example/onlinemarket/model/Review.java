571720fab0ce6037143c6ed9666c7dab3e85a2ba
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-17 08:51:33 -0800

    Adding review model class.

---
+++ app/src/main/java/com/example/onlinemarket/model/Review.java
@@ -0,0 +1,69 @@
+package com.example.onlinemarket.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Review{
+
+	@SerializedName("review")
+	private String review;
+
+	@SerializedName("product_id")
+	private int productId;
+
+	@SerializedName("rating")
+	private int rating;
+
+	@SerializedName("reviewer")
+	private String reviewer;
+
+	@SerializedName("reviewer_email")
+	private String reviewerEmail;
+
+	public Review(String review, int productId, int rating, String reviewer, String reviewerEmail) {
+		this.review = review;
+		this.productId = productId;
+		this.rating = rating;
+		this.reviewer = reviewer;
+		this.reviewerEmail = reviewerEmail;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getReviewer() {
+		return reviewer;
+	}
+
+	public void setReviewer(String reviewer) {
+		this.reviewer = reviewer;
+	}
+
+	public String getReviewerEmail() {
+		return reviewerEmail;
+	}
+
+	public void setReviewerEmail(String reviewerEmail) {
+		this.reviewerEmail = reviewerEmail;
+	}
+}
