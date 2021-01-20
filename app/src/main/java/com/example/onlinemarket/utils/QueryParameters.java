64dfb7e724868ed9f9e23d9daa02591a9f52ff5f
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-20 07:28:37 -0800

    bugfix!: into receive special products.

--- app/src/main/java/com/example/onlinemarket/utils/QueryParameters.java
+++ app/src/main/java/com/example/onlinemarket/utils/QueryParameters.java
@@ -3,7 +3,7 @@ package com.example.onlinemarket.utils;
 public class QueryParameters {
     public static final String ORDER_BY="orderby";
     public static final String ORDER="order";
-    public static final String ON_SALE="onSale";
+    public static final String ON_SALE="on_sale";
     public static final String ATTRIBUTE="attribute";
     public static final String ATTRIBUTE_TERM="attribute_term";
     public static final String SEARCH="search";
