1612bc283a9b0f848fd638e9216e09e0049fb535
FatemeAfshar20 <uttab4760@gmail.com>
2021-01-20 16:22:15 -0800

    Complete get location on google map and zoom on address.

--- app/src/main/java/com/example/onlinemarket/view/fragment/MapFragment.java
+++ app/src/main/java/com/example/onlinemarket/view/fragment/MapFragment.java
@@ -9,9 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.onlinemarket.R;
+import com.example.onlinemarket.model.CustomerLocation;
+import com.example.onlinemarket.sharePref.OnlineShopSharePref;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,7 @@ import com.google.android.gms.maps.SupportMapFragment;
  * create an instance of this fragment.
  */
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -35,10 +41,20 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+/*        mMap.setMinZoomPreference(6.0f);
+        mMap.setMaxZoomPreference(14.0f);*/
 
+        CustomerLocation customerLocation=
+                OnlineShopSharePref.getCustomerLastedLocation(getActivity());
+
+        LatLng latLng = new LatLng(customerLocation.getLatitude(), customerLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(latLng).title("my location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,12.0f));
     }
 }
